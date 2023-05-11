405f14695165aaf0ba61ddc7d88aabdd0ddd55af
Marwamu <marwamohamed12922gmail.com>
2023-05-11 22:23:10 +0300

    Features Style

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ const menuItems = document.querySelectorAll(".menuItem")
 menuItems.forEach((item, index) => {
     item.addEventListener('click', () => {
         // X- axis strat 0 then when click in menuitem increase -100vw to show next slide shoe
+        var itemPrev = document.querySelector(".active");
+        console.log(itemPrev);
         wrapper.style.transform = `translateX(${-100 * index}vw)`
-
+        itemPrev.classList.remove("active")
+        item.classList.add("active")
     })
 });
-item.style.color = `white`
